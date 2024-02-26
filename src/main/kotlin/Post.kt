21be0ba9adcab8a2549855af34b7078d5fc1de6e
MaestroMikael <153710935+MaestroMikael@users.noreply.github.com>
2024-02-26 20:58:53 +0300

    fixed Post and Test

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -6,8 +6,9 @@ data class Post(
     val date: Long?,
     val text: String?,
     val likes: Likes = Likes(),
-    val attachment: WallService.Attachment
+    val attachments: List<Attachment>
 )
+
 data class Likes(var count: Int = 0)
 
 object WallService {
@@ -47,23 +48,33 @@ object WallService {
         return false
     }
 
-    open class Attachment(val type: String)
-
-    data class PhotoAttachment(val url: String, val description: String) : Attachment("photo")  // фото наследник
-
-    data class AudioAttachment(val title: String, val artist: String) : Attachment("audio")  // аудио наследник
-
-    data class VideoAttachment(val url: String, val duration: Int) : Attachment("video") //видео наследник
 }
+
+open class Attachment(val type: String)
+
+data class PhotoAttachment(val url: String, val description: String) : Attachment("photo")  // фото наследник
+
+data class AudioAttachment(val title: String, val artist: String) : Attachment("audio")  // аудио наследник
+
+data class VideoAttachment(val url: String, val duration: Int) : Attachment("video") //видео наследник
+
+data class DocumentAttachment(val url: String, val author: String) : Attachment("document")
+
+data class LinktAttachment(val url: String, val title: String) : Attachment("link")
+
 
 fun main() {
 
-    val photoAttachment = WallService.PhotoAttachment("https://sun9-34.userapi.com/impg/lRpzBSt7gMvFPve0F35m9TWWPpnKaNfo7MEecw/gmAHHBUY_XQ.jpg?size=1439x2160&quality=95&sign=ad479ad94a59174119d0ce48397ff890&type=album", "MySon")
-    val videoAttachment = WallService.VideoAttachment("https://vk.com/video-52671851_456250043", 180)
-
+    val attachments = listOf(
+        PhotoAttachment(
+            "https://sun9-67.userapi.com/impg/P1fDGDk2DuW9wtva2Mg0yFO5O44fdIjrjRTFWw/EC2UsluBYQQ.jpg?size=853x1280&quality=95&sign=ae54302b4ccc9793cf5b85adde759a33&type=album",
+            "Mark"
+        ),
+        AudioAttachment("Название аудио", "Исполнитель"),
+        VideoAttachment("http://example.com/video1", 120)
+    )
     val likes = Likes(99)
-    WallService.add(Post(1, "Me", 2983, "hello post", likes, photoAttachment))
-    WallService.updatePost(Post(1, "Me", 2955, "hello post", likes, videoAttachment))
+    WallService.add(Post(1, "Me", 2983, "hello post", likes, attachments))
 
     WallService.clearPostId(1)
     WallService.postPrint()
--- src/test/kotlin/WallTest.kt
+++ src/test/kotlin/WallTest.kt
@@ -8,24 +8,30 @@ class WallServiceTest {
     fun setUp() {  // Инициализируем настройки теста
         WallService.posts = emptyArray()
         WallService.lastId = 0
-
     }
 
+    val attachments = listOf(
+        PhotoAttachment(
+            "https://sun9-67.userapi.com/impg/P1fDGDk2DuW9wtva2Mg0yFO5O44fdIjrjRTFWw/EC2UsluBYQQ.jpg?size=853x1280&quality=95&sign=ae54302b4ccc9793cf5b85adde759a33&type=album",
+            "Mark"
+        ),
+        AudioAttachment("Название аудио", "Исполнитель"),
+        VideoAttachment("http://example.com/video1", 120)
+    )
     @Test
     fun testAddPost() { // Тест на дол=бавление поста
-        val photoAttachment = WallService.PhotoAttachment("image.jpg", "Description")
-        val post = Post(1, "Me", 2983, "hello post", Likes(100),photoAttachment)
+
+        val post = Post(1, "Me", 2983, "hello post", Likes(100),attachments)
         val addedPost = WallService.add(post)
         assertEquals(1, addedPost.id)
     }
 
     @Test
     fun testUpdatePost() {  // Тест на обновление
-        val photoAttachment = WallService.PhotoAttachment("image.jpg", "Description")
-        val initialPost = Post(1, "Me", 2983, "hello post", Likes(100),photoAttachment)
+        val initialPost = Post(1, "Me", 2983, "hello post", Likes(100),attachments)
         WallService.add(initialPost)
 
-        val updatedPost = Post(1, "Me", 2483, "hello post 2", Likes(100),photoAttachment)
+        val updatedPost = Post(1, "Me", 2483, "hello post 2", Likes(100),attachments)
         val isUpdated = WallService.updatePost(updatedPost)
         assertTrue(isUpdated)
 
@@ -35,8 +41,7 @@ class WallServiceTest {
 
     @Test
     fun testClearPostId() {
-        val photoAttachment = WallService.PhotoAttachment("image.jpg", "Description")
-        val post = Post(1, "Me", 2983, "hello post", Likes(100),photoAttachment)
+        val post = Post(1, "Me", 2983, "hello post", Likes(100),attachments)
         WallService.add(post)
 
         val isCleared = WallService.clearPostId(1)
